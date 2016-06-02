d7dce897e2c04f17e3ccfad6370724697fbccdd5
Roman <meyta@incom.tomsk.ru>
2016-06-02 14:36:11 +0600

    Добавил обработку ключей для dataKeeper

--- main.cpp
+++ main.cpp
@@ -3,12 +3,33 @@
  * Incom inc Tomsk Russia http://incom.tomsk.ru/
  */
 
+#include <iostream>
+#include <unistd.h>
+
 #include "KeeperApplication.h"
 
 using namespace std;
 
-int main() {
+int main(int argc, char** argv) {
+    int rez = 0;
+    string config("/etc/dataKeeper.conf");
+    while ( (rez = getopt(argc,argv,"c:h")) != -1 ) {
+        switch (rez) {
+            case 'c':
+                config = optarg;
+                break;
+            case 'h':
+                cout << "dataKeeper - plugin based database manager" << endl;
+                cout << "Usage: dataKeeper [options]" << endl;
+                cout << "-c <path_to_config> - path to configuration file" << endl;
+                cout << "-h - print this help" << endl;
+                return 0;
+            default:
+                break;
+        }
+    }
+
     KeeperApplication& instance = KeeperApplication::getInstance();
-    instance.loadDatabasePlugins("plugins_conf.json");
+    instance.loadDatabasePlugins(config);
     return instance.execute();
 }
